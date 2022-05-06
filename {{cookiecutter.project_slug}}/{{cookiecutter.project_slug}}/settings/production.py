da97c8848992ba842bddaba0a2d6774b79271c03
Stephen Turoscy <sturoscy@globus.org>
2022-05-06 09:40:05 -0400

    Adding default endpoint

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/settings/production.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/settings/production.py
@@ -8,7 +8,9 @@ store = EC2ParameterStore(
 parameters = store.get_parameters_by_path("/django/django_globus_app/", strip_path=True)
 
 DEBUG = True
-PORTAL_ENDPOINT_ID = globus_sdk.LocalGlobusConnectPersonal().endpoint_id
+PORTAL_ENDPOINT_ID = parameters.get(
+    "PORTAL_ENDPOINT_ID", globus_sdk.LocalGlobusConnectPersonal().endpoint_id
+)
 SECRET_KEY = parameters.get("SECRET_KEY")
 SOCIAL_AUTH_GLOBUS_KEY = parameters.get("SOCIAL_AUTH_GLOBUS_KEY")
 SOCIAL_AUTH_GLOBUS_SECRET = parameters.get("SOCIAL_AUTH_GLOBUS_SECRET")
